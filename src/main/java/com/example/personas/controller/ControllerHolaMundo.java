a962ca336b3a65075a1e93d78a4393761908723a
gustavocolmar-crypto <gustavo.colmar@gmail.com>
2026-09-10 11:32:15 -0600

    holas

--- src/main/java/com/example/personas/controller/ControllerHolaMundo.java
+++ src/main/java/com/example/personas/controller/ControllerHolaMundo.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerHolaMundo {
 
-    @GetMapping("/hola")
+    @GetMapping("/holas")
     public String holaMundo(){
         return "holaMundo";
     }
