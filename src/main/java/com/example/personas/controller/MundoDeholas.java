c279d9246010c8d1f13b26aa09bc1e7a8f98f305
gustavocolmar-crypto <gustavo.colmar@gmail.com>
2026-09-10 11:45:48 -0600

    nuevaclase

--- src/main/java/com/example/personas/controller/MundoDeholas.java
+++ src/main/java/com/example/personas/controller/MundoDeholas.java
@@ -1,4 +1,11 @@
 package com.example.personas.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class MundoDeholas {
+
+    @GetMapping("/nuevo")
+    public String holaMundo(){
+        return "holaMundo";
+    }
 }
