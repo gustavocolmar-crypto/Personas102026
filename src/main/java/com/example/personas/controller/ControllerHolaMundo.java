b7eb05fbe7ea244ecd62a220ea60013cc1e0ffc6
Jordan <jordancolorado@go.tecmoin.com>
2026-09-10 11:39:34 -0600

    commit desde intelliJ2

--- src/main/java/com/example/personas/controller/ControllerHolaMundo.java
+++ src/main/java/com/example/personas/controller/ControllerHolaMundo.java
@@ -22,4 +22,9 @@ public class ControllerHolaMundo {
         return "commit desde git";
     }
 
+    @GetMapping("/pruebaGit4")
+    public String prueba4(){
+        return "commit desde git";
+    }
+
 }
